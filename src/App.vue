f75973e9370c78e5ec42bef1b10ec8f0acdf9e15
Valijon <parliament888@gmail.com>
2017-03-05 22:54:54 +0800

    Setting up routes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="app">
-
+        <div class="container">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import { routes } from './routes'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
 
 new Vue({
     el: '#app',
     template: '<App/>',
-    components: { App }
+    components: { App },
+    router
 })
