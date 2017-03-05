d6154ad5ed9792dbec0d0bd61fd9efabeee9c0ad
Valijon <parliament888@gmail.com>
2017-03-05 23:11:12 +0800

    Header and Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
     <div id="app">
         <div class="container">
-            <router-view></router-view>
+            <app-header></app-header>
+            <div class="row">
+                <div class="col-xs-12">
+                    <router-view></router-view>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Header from './components/Header'
     export default {
         name: 'app',
-        components: { }
+        components: {
+            appHeader: Header
+        }
     }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,37 @@
 <template>
-    <h1>Header Компонент</h1>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-lin to="/" class="navbar-brand">Продавец акций</router-lin>
+            </div>
+
+            <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <router-link to="/portfolio" activeClass="active" tag="li"><a>Портфолио</a></router-link>
+                <router-link to="/stocks" activeClass="active" tag="li"><a>Акции</a></router-link>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Конец Дня</a></li>
+                <li class="dropdown">
+                <a
+                    href="#"
+                    class="dropdown-toggle"
+                    data-toggle="dropdown"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false">Сохранить & Загрузить <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                </ul>
+                </li>
+            </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 </template>
 
 <script>
